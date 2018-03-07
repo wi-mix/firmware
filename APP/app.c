51376c9dd74e5934cf3fe5dbfbe9607bbd19b2a2
Ryan Huard <ryan.huard@live.ca>
2018-03-07 16:24:03 -0700

    Remove http parsing

--- APP/app.c
+++ APP/app.c
@@ -212,71 +212,6 @@ int main ()
 }
 
 
-static const char data[] =
-    "POST /joyent/http-parser HTTP/1.1\r\n"
-    "Host: github.com\r\n"
-    "DNT: 1\r\n"
-    "Accept-Encoding: gzip, deflate, sdch\r\n"
-    "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\r\n"
-    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/39.0.2171.65 Safari/537.36\r\n"
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,"
-        "image/webp,*/*;q=0.8\r\n"
-    "Referer: https://github.com/joyent/http-parser\r\n"
-    "Connection: keep-alive\r\n"
-    "Transfer-Encoding: chunked\r\n"
-    "Cache-Control: max-age=0\r\n\r\nb\r\nhello world\r\n0\r\n\r\n";
-static const size_t data_len = sizeof(data) - 1;
-
-static int on_info(http_parser* p) {
-	printf("On_info\r\n");
-	return 0;
-}
-
-
-static int on_data(http_parser* p, const char *at, size_t length) {
-	char data[length + 1];
-	memset(data, 0, length + 1);
-	snprintf(data, length+1, "%s", at);
-	printf("On_data: %s\r\n", data);
-	return 0;
-}
-
-static http_parser_settings settings = {
-  .on_message_begin = on_info,
-  .on_headers_complete = on_info,
-  .on_message_complete = on_info,
-  .on_header_field = on_data,
-  .on_header_value = on_data,
-  .on_url = on_data,
-  .on_status = on_data,
-  .on_body = on_data
-};
-
-int bench(int iter_count, int silent) {
-  struct http_parser parser;
-  int i;
-  int err;
-
-  printf("Entered parser\r\n");
-  for (i = 0; i < iter_count; i++) {
-	  printf("Parse %d\r\n", i);
-    size_t parsed;
-    http_parser_init(&parser, HTTP_REQUEST);
-
-    parsed = http_parser_execute(&parser, &settings, data, data_len);
-  }
-
-  if (!silent) {
-
-    fprintf(stdout, "Benchmark result:\n");
-
-    fflush(stdout);
-  }
-
-  return 0;
-}
 
 /*
 *********************************************************************************************************
@@ -315,7 +250,6 @@ static  void  AppTaskStart (void *p_arg) {
     for(;;) {
         OSTimeDlyHMSM(0, 0, 10, 000);
 
-        bench(1, 0);
     }
 
 }
