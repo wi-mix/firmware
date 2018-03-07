5ad04ceb80dcaa9e6af32194d229f573af2e28da
Ryan Huard <ryan.huard@live.ca>
2018-03-07 14:37:16 -0700

    Business models for firmware

---
+++ APP/models/models.h
@@ -0,0 +1,23 @@
+#ifndef MODELS_H
+#define MODELS_H
+typedef struct ingredient
+{
+    int id,
+    short amount,
+    short order
+} ingredient;
+
+typedef struct recipe
+{
+    ingredient ingredients[3],
+    unsigned char ordered,
+    unsigned char ingredient_count,
+} recipe;
+
+typedef struct canister
+{
+    int canister_id,
+    ingredient * ingredient
+} canister;
+
+#endif //MODELS_H
